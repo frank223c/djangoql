6f80428cc7f3e2e8bbe0ef7cd408838b7a2f52b0
Jonathan McAuliffe <watch.n.learn@gmail.com>
2017-08-18 00:16:34 +0200

    admin search box adapated for grappelli interface

--- djangoql/static/djangoql/js/completion_admin.js
+++ djangoql/static/djangoql/js/completion_admin.js
@@ -1,32 +1,42 @@
 (function (DjangoQL) {
-  'use strict';
+    'use strict';
 
-  DjangoQL.DOMReady(function () {
-    // Replace standard search input with textarea
-    var textarea;
-    var input = document.querySelector('input[name=q]');
-    if (!input) {
-      return;
-    }
-    textarea = document.createElement('textarea');
-    textarea.value = input.value;
-    textarea.id = input.id;
-    textarea.name = input.name;
-    textarea.rows = 2;
-    textarea.style.height = "25px";
-    textarea.style.borderRadius = "10px";
-    textarea.setAttribute('maxlength', 2000);
-    input.parentNode.insertBefore(textarea, input);
-    input.parentNode.removeChild(input.nextSibling);
-    input.parentNode.removeChild(input);
-    textarea.focus();
+    DjangoQL.DOMReady(function () {
+        // Replace standard search input with textarea
+        var textarea;
+        var input = document.querySelector('input[name=q]');
+        var form = document.querySelector('#grp-changelist-search-form')
 
+        if (!input) {
+          return;
+        }
 
-    DjangoQL.init({
-      introspections: 'introspect/',
-      syntaxHelp: 'djangoql-syntax/',
-      selector: 'textarea[name=q]',
-      autoResize: true
+        textarea = document.createElement('textarea');
+        textarea.value = input.value;
+        textarea.id = input.id;
+        textarea.name = input.name;
+        textarea.rows = 2;
+        textarea.style.height = "25px";
+        textarea.style.width = "400px";
+        textarea.style.float = "right";
+        textarea.style.borderRadius = "5px";
+        textarea.setAttribute('maxlength', 2000);
+
+        input.parentNode.insertBefore(textarea, input);
+        input.parentNode.removeChild(input.nextSibling);
+        input.parentNode.removeChild(input);
+
+        form.style.width = "404px"
+        form.style.border = "0px"
+        form.style.float = "right"
+
+        textarea.focus();
+
+        DjangoQL.init({
+            introspections: 'introspect/',
+            syntaxHelp: 'djangoql-syntax/',
+            selector: 'textarea[name=q]',
+            autoResize: true
+        });
     });
-  });
 }(window.DjangoQL));
